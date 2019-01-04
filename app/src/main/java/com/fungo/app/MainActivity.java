91570ae9b5f5af9f5571478341dedff564e78b5b
Pinger <fungoit@gmail.com>
2019-01-04 12:15:56 +0800

    添加app测试

---
+++ app/src/main/java/com/fungo/app/MainActivity.java
@@ -0,0 +1,25 @@
+package com.fungo.app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tencent.mta.track.StatisticsDataAPI;
+
+
+/**
+ * 可视化埋点
+ *
+ * @see [https://mta.qq.com/docs/app_advance_access_visual_burial.html]
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 可视化sdk初始化
+        StatisticsDataAPI.instance(this);
+
+    }
+}
